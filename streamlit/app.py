6461535976f614514e0a13196ee9d30e95f74f81
agkavin <agkavin7@gmail.com>
2024-08-18 15:55:47 +0530

    modified:   streamlit/app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,23 +1,6 @@
 import streamlit as st
 from PIL import Image
 
-# # Define a function to display the sidebar and handle page selection
-# def display_sidebar():
-#     st.sidebar.title("Navigation")
-#     pages = {
-#         "Complaint Recognition": "complaint_recognition",
-#         "Complaint Classification": "complaint_classification",
-#         "Preliminary Response": "preliminary_response",
-#         # Add more pages here as needed
-#     }
-#     selection = st.sidebar.radio("Select a page", list(pages.keys()))
-#     return pages[selection]
-
-# Load the selected page
-# def load_page(page_name):
-#     # Dynamic import of the page module
-#     page = __import__(f'pages.{page_name}', fromlist=[''])
-#     page.app()
 
 def input():
 
